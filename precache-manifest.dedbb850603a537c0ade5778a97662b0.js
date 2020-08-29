45e26c25e779b94bf8ac64dd7d04f51ccd947925
muhammad umer farooq <umer.farooq6028@gmail.com>
2020-08-29 23:07:15 +0500

    Updates

---
+++ precache-manifest.dedbb850603a537c0ade5778a97662b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b0cc0209cc87edd436ac423ff1be4f",
+    "url": "/monsters-rolodex.git/index.html"
+  },
+  {
+    "revision": "002353a936b2fcdf1209",
+    "url": "/monsters-rolodex.git/static/css/main.c5ab5a79.chunk.css"
+  },
+  {
+    "revision": "0c93387514e954bdf57e",
+    "url": "/monsters-rolodex.git/static/js/2.605ea4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex.git/static/js/2.605ea4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "002353a936b2fcdf1209",
+    "url": "/monsters-rolodex.git/static/js/main.4b6a251c.chunk.js"
+  },
+  {
+    "revision": "17eab2e137afb3f838f1",
+    "url": "/monsters-rolodex.git/static/js/runtime-main.ccb7814c.js"
+  }
+]);
